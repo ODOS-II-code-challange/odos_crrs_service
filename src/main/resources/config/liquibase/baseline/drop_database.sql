40fc77c0f6fb4835039781c3f9c83a0d8e875e54
Wube Kifle <Wubshet.kifle@newwave.io>
2018-04-17 10:37:05 -0400

    ODOS_20:"Fixed liquibase baseline and hibernate sequence"

--- src/main/resources/config/liquibase/baseline/drop_database.sql
+++ src/main/resources/config/liquibase/baseline/drop_database.sql
@@ -1,1 +1,6 @@
+UPDATE pg_database SET datallowconn = 'fasle' WHERE datname = :databaseNameQuotes 
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = :databaseNameQuotes 
+
 DROP DATABASE IF EXISTS :databaseName;
+
+UPDATE pg_database SET datallowconn = 'true' WHERE datname = :databaseNameQuotes 
--- src/main/resources/config/liquibase/baseline/drop_user.sql
+++ src/main/resources/config/liquibase/baseline/drop_user.sql
@@ -1,3 +1,3 @@
-revoke all on database :databaseName from :databaseAppRole;
-
+REVOKE ALL ON DATABSE :databaseName from :databaseAppRole;
+DROP USER IF EXISTS :databaseAppUser;
 DROP ROLE IF EXISTS :databaseAppRole;

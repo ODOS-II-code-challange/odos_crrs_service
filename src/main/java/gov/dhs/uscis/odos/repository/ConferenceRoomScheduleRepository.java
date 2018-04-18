b47c8361192a1f4503a19a6036c1ae2aa36a91dd
Wube Kifle <Wubshet.kifle@newwave.io>
2018-04-18 01:54:44 -0400

    ODOS_20:Added ConferenceRoomSchedule repo and service

---
+++ src/main/java/gov/dhs/uscis/odos/repository/ConferenceRoomScheduleRepository.java
@@ -0,0 +1,15 @@
+package gov.dhs.uscis.odos.repository;
+
+import gov.dhs.uscis.odos.domain.ConferenceRoomSchedule;
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.jpa.repository.*;
+
+
+/**
+ * Spring Data JPA repository for the ConferenceRoomSchedule entity.
+ */
+@Repository
+public interface ConferenceRoomScheduleRepository extends JpaRepository<ConferenceRoomSchedule, Long> {
+
+}

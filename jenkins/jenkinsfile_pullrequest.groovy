2b26d6fa297325a69ef1e7beeaab03f18ab789ff
Wube Kifle <Wubshet.kifle@newwave.io>
2018-04-20 00:39:46 -0400

    ODOS_PR:Updated sonar command

--- jenkins/jenkinsfile_pullrequest.groovy
+++ jenkins/jenkinsfile_pullrequest.groovy
@@ -20,18 +20,24 @@ pipeline {
               Common.slack 'Sonar Scan for build request...'
               //Common.sonarScan()
               withCredentials([usernamePassword(credentialsId: 'soar-login', passwordVariable: 'SONAR_PASSWORD', usernameVariable: '')]) {
-                  sh "./gradlew --full-stacktrace\
-					-Dsonar.host.url=http://odos.lassiterdynamics.com:9000 \
-					-Dsonar.github.endpoint=https://api.github.com \
-					-Dsonar.github.repository=ODOS-II-code-challange/odos_crrs_service  \
-					-Dsonar.projectKey=gov.dhs.uscis.odos:crrsvc \
-					-Dsonar.analysis.mode=preview \
-					-Dsonar.github.pullRequest=$ghprbPullId \
-					-Dsonar.github.repository=$ghprbGhRepository \
-					-Dsonar.github.login=odos-jenkins \
-					-Dsonar.github.oauth=c1d702b375ef96065d3e4fa6996681033fbbf1e2 \
-                    -Dsonar.login=jenkins \
-					-Dsonar.password=$SONAR_PASSWORD"
+                   withCredentials([usernamePassword(credentialsId: 'soar-login', passwordVariable: 'SONAR_PASSWORD', usernameVariable: '')]) {
+                  sh """
+                        ./gradlew --full-stacktrace\
+                        -Dsonar.host.url=http://odos.lassiterdynamics.com:9000 \
+                        -Dsonar.github.endpoint=https://api.github.com \
+                        -Dsonar.github.repository=ODOS-II-code-challange/odos_crrs_service  \
+                        -Dsonar.projectKey=gov.dhs.uscis.odos:crrsvc \
+                        -Dsonar.analysis.mode=preview \
+                        -Dsonar.github.pullRequest=$ghprbPullId \
+                        -Dsonar.github.repository=$ghprbGhRepository \
+                        -Dsonar.github.login=odos-jenkins \
+                        -Dsonar.github.oauth=c1d702b375ef96065d3e4fa6996681033fbbf1e2 \
+                        -Dsonar.login=jenkins \
+                        -Dsonar.password=$SONAR_PASSWORD
+                        -x test sonarqube
+                    """
+   
+            }
    
             }
                
